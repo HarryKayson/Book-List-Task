37b4d33b941898b677811aa28c963d2b79f8df15
HarryKayson <77840620+HarryKayson@users.noreply.github.com>
2024-12-14 14:16:24 +0300

    Add files via upload

---
+++ Book list.py
@@ -0,0 +1,7 @@
+#Task
+#Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
+favorite_books=("Rich Dad Poor Dad", "The Alchemist", "Pychocybernetics", "Think and Grow Rich", "Men are from Mars and Women are from Venus")
+ 
+#Usng the for loop to print each book name on a seperate line
+for book in favorite_books:
+    print(book)
